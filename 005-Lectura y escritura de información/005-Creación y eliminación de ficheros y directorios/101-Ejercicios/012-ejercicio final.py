b07df6ef5e36b5c2054e2e3ca96e201aecc61b3c
Jose Vicente Carratala <info@josevicentecarratala.com>
2025-10-21 12:08:54 +0200

    1

---
+++ 005-Lectura y escritura de información/005-Creación y eliminación de ficheros y directorios/101-Ejercicios/012-ejercicio final.py
@@ -0,0 +1,9 @@
+'''
+  Quiero:
+  1.-Pedir al usuario una ruta de una carpeta con input
+  2.-Repasar todas las subcarpetas y archivos dentro de esa carpeta
+  3.-Para cada archivo o carpeta, quiero comprimirla en un ZIP
+  4.-Una vez comprimido ese zip, quiero eliminar los contenidos originales
+'''
+
+
