4b2d7df4a94dced279f6e13e0536200703183f7a
Jose Vicente Carratala <info@josevicentecarratala.com>
2025-10-24 09:11:09 +0200

    Update 006-mysql.py

--- 005-Lectura y escritura de información/008-Concepto de evento/101-Ejercicios/006-mysql.py
+++ 005-Lectura y escritura de información/008-Concepto de evento/101-Ejercicios/006-mysql.py
@@ -1,23 +1,23 @@
+# pip3 install mysql-connector-python --break-system-packages
+# sudo apt install libmysqlclient-dev python3-mysql.connector
 import mysql.connector
-
 conexion = mysql.connector.connect(
     host="localhost",
     user="empresadam",
-    password="Accesoadatos2526$",
+    password="Empresadam123$",
     database="empresadam"
 )
 cursor = conexion.cursor()
 cursor.execute('''
   INSERT INTO clientes
   VALUES(
-    1,
+    NULL,
+    "12345678Z",
     "Jose Vicente",
     "Carratala Sanchis",
     "info@jocarsa.com"
   );
 ''')
-
 conexion.commit()
-
 cursor.close()
 conexion.close()
